1341e61a46af012375065e38b2114fa1b8711f17
Rohan de Swardt <rohanza@gmail.com>
2017-03-12 15:08:34 +0200

    update package classifiers with tested python versions

--- setup.py
+++ setup.py
@@ -47,7 +47,10 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     packages=[
         'pandora',
